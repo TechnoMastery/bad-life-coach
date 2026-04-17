c5132997803c3b99c36916b7ac9616b7f977191a
benny232-232 <benjamincailliez2303@gmail.com>
2026-04-17 11:08:37 +0200

    added advises

--- src/main/java/fr/benny/badlifecoach/BadLifeAdviseHandler.java
+++ src/main/java/fr/benny/badlifecoach/BadLifeAdviseHandler.java
@@ -8,7 +8,13 @@ public class BadLifeAdviseHandler {
     private static final List<String> advises = new ArrayList<>();
 
     public static void fillAdvises() {
-
+        advises.add("T’as encore rien fait aujourd’hui");
+        advises.add("Va prendre une douche");
+        advises.add("Envoie-lui un message");
+        advises.add("Fais tes devoirs");
+        advises.add("Arrête ton téléphone");
+        advises.add("Bois de l’eau");
+        advises.add("Va dormir");
     }
 
     public static void generateAdvise() {
