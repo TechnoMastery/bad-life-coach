c47a8451f9e0de719de702ea545d5461bd1cebd5
benny232-232 <benjamincailliez2303@gmail.com>
2026-04-17 11:22:10 +0200

    adder advise display

--- src/main/java/fr/benny/badlifecoach/BadLifeAdviseHandler.java
+++ src/main/java/fr/benny/badlifecoach/BadLifeAdviseHandler.java
@@ -1,13 +1,20 @@
 package fr.benny.badlifecoach;
 
+import net.minheur.potoflux.PotoFlux;
+
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class BadLifeAdviseHandler {
 
     private static final List<String> advises = new ArrayList<>();
+    private static final Random random = new Random();
 
     public static void fillAdvises() {
+        advises.clear();
+
         advises.add("T’as encore rien fait aujourd’hui");
         advises.add("Va prendre une douche");
         advises.add("Envoie-lui un message");
@@ -18,7 +25,13 @@ public class BadLifeAdviseHandler {
     }
 
     public static void generateAdvise() {
+        String advise = advises.get(random.nextInt(advises.size()));
 
+        JOptionPane.showMessageDialog(
+                PotoFlux.app.getFrame(),
+                advise, "Voici un conseil:",
+                JOptionPane.INFORMATION_MESSAGE
+        );
     }
 
 }
